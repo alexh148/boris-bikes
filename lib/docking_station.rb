fa5dcd000cadca907e00df952bc90f9cedc89dbc
your name <saule.guzyte@gmail.com>
2019-01-09 14:37:18 +0000

    Task 11 completed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,8 +2,20 @@ require_relative 'bikes'
 
 class DockingStation
 
+def initialize
+  @bike = "Dock is empty"
+end
+
+attr_reader :bike
+
   def release_bike
     Bikes.new
   end
 
+  def dock(bike_to_be_added)
+    @bike = bike_to_be_added
+  end
+
 end
+
+#DockingStation.dock(newbike)
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -11,4 +11,24 @@ describe DockingStation do
     expect(bike).to be_working
   end
 
+  it "responds to the method dock" do
+    # bike = Bikes.new
+    # docking_station = DockingStation.new
+    expect(subject).to respond_to :dock
+  end
+
+  it "adds a new bike to the docking_station" do
+    bike1 = Bikes.new
+    expect(subject.dock(bike1)).to eq bike1
+
+    # docking_station = DockingStation.new
+    # bike1 = docking_station.release_bike
+    # docking_station.dock(bike1)
+    # expect(docking_station.bike).to eq (bike1)
+
+
+  end
+
+
+
 end
